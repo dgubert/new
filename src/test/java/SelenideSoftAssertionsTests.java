774122f6289cd62b19c27ea4674aadb04be59b1a
Test DG User <Test DG Email>
2024-09-10 22:30:14 +0300

    fix test

--- src/test/java/SelenideSoftAssertionsTests.java
+++ src/test/java/SelenideSoftAssertionsTests.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -5,11 +7,32 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideSoftAssertionsTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920*1080";
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
     @Test
     void junit5ShouldBeOnSoftAssertionsPage() {
-        open("https://github.com/selenide/selenide");
+        open("/selenide/selenide");
         $("[data-content='Wiki']").click();
         $("#wiki-body").$("[href*='SoftAssertions'").click();
-        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """));
     }
 }
